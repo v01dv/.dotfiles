3a61ef1d63025922f703cb03c6be72d632b5b107
voldv <gt.en72v@simplelogin.co>
2026-09-21 22:49:28 +0300

    feat(wezterm): minor update

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,7 +7,7 @@ return {
 	window_decorations = "RESIZE",
 	color_scheme = "Catppuccin Frappe",
 	font = wezterm.font_with_fallback({
-		{ family =  "Cascadia Code", harfbuzz_features = { 'ss01=1', 'ss02=1', 'ss19=1', 'ss20=1'}, },
+		{ family = "Cascadia Code", harfbuzz_features = { "ss01=1", "ss02=1", "ss19=1", "ss20=1" } },
 		{ family = "Symbols Nerd Font Mono", scale = 0.75 },
 	}),
 	window_background_opacity = 1.0,
@@ -16,11 +16,12 @@ return {
 	font_size = 10,
 	scrollback_lines = 10000,
 	hide_tab_bar_if_only_one_tab = true,
+	max_fps = 120,
 
-   window_padding = {
-    left = 1,
-    right = 1,
-    top = 1,
-    bottom = 1,
-  },
+	window_padding = {
+		left = 1,
+		right = 1,
+		top = 1,
+		bottom = 1,
+	},
 }
